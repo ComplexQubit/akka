8d4603f042310f2b8c8993d4ce39fbcd24a03f18
Roland Kuhn <rk@rkuhn.info>
2014-03-19 09:34:26 +0100

    +act,rem,mul #3930 track UID for “live” associations
    The problem was that a system could go away, and before the connection
    is cleared out a restarted system at the same address could connect
    back, which would confuse the endpoints: it would attempt a take-over of
    the new incoming connection by the old EndpointWriter, which is bogus.
    The fix is to kill the old ReliableDeliverySupervisor in this case and
    start afresh.
    
    Also in this episode:
    
    - actually stop actors which are “replaced” by the RemoteDaemon
      (otherwise would leak; should not happen, but ...)
    - watch the parent of remote-deployed actors in the RemoteDaemon to
      clean out the children in case the old system goes away without
      publishing AddressTerminated (see main scenario above)
    - improve MultiNodeSpec to allow injection of configured deployments
      into arbitrary ActorSystems
    - add label support to LoggingReceive for become-based state machines
    - document that it is intentional to swallow Disassociated within the
      ThrottlerTransportAdapter
    - include number of received elements in the timeout failure message for
      TestKit.receiveN

--- akka-actor/src/main/scala/akka/event/LoggingReceive.scala
+++ akka-actor/src/main/scala/akka/event/LoggingReceive.scala
@@ -36,17 +36,30 @@ object LoggingReceive {
    * This is an EXPERIMENTAL feature and is subject to change until it has received more real world testing.
    */
   def create(r: Receive, context: ActorContext): Receive = apply(r)(context)
+
+  /**
+   * Create a decorated logger which will append `" in state " + label` to each message it logs.
+   */
+  def withLabel(label: String)(r: Receive)(implicit context: ActorContext): Receive = r match {
+    case _: LoggingReceive ⇒ r
+    case _                 ⇒ if (context.system.settings.AddLoggingReceive) new LoggingReceive(None, r, Some(label)) else r
+  }
 }
 
 /**
  * This decorator adds invocation logging to a Receive function.
  * @param source the log source, if not defined the actor of the context will be used
  */
-class LoggingReceive(source: Option[AnyRef], r: Receive)(implicit context: ActorContext) extends Receive {
+class LoggingReceive(source: Option[AnyRef], r: Receive, label: Option[String])(implicit context: ActorContext) extends Receive {
+  def this(source: Option[AnyRef], r: Receive)(implicit context: ActorContext) = this(source, r, None)
   def isDefinedAt(o: Any): Boolean = {
     val handled = r.isDefinedAt(o)
     val (str, clazz) = LogSource.fromAnyRef(source getOrElse context.asInstanceOf[ActorCell].actor)
-    context.system.eventStream.publish(Debug(str, clazz, "received " + (if (handled) "handled" else "unhandled") + " message " + o))
+    context.system.eventStream.publish(Debug(str, clazz, "received " + (if (handled) "handled" else "unhandled") + " message " + o
+      + (label match {
+        case Some(l) ⇒ " in state " + l
+        case _       ⇒ ""
+      })))
     handled
   }
   def apply(o: Any): Unit = r(o)
